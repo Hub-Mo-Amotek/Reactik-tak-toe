85c5a2ebd629b90e00a50a70d95997edd348b1ec
Mo <Mohamed.elja@hotmail.com>
2024-07-09 19:34:45 +0200

    Cleans up current components code

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from 'react'
-import Board from './Board'
+import React, { useState } from 'react';
+import Board from './Board';
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
+  const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
-  }
+  };
 
-function jumpTo(nextMove) {
-  setCurrentMove(nextMove);
-  setXIsNext(nextMove % 2 === 0);
-}
+  const jumpTo = (move) => {
+    setCurrentMove(move);
+  };
 
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
+  const moves = history.map((_, move) => {
+    const description = move ? `Go to move #${move}` : 'Go to game start';
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
@@ -35,17 +27,15 @@ function jumpTo(nextMove) {
   });
 
   return (
-    <div>
-      <div className="game">
-        <div className="game-board">
-          <Board xIsNext ={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-        </div>
-        <div className="game-info">
-          <ol>{moves}</ol>
-        </div>
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,11 @@
+import React from 'react';
 
 const Square = ({ value, onSquareClick }) => {
-
-
   return (
-    <div>
-          <button className="square" onClick={onSquareClick}>
-              {value}
-          </button>
-    </div>
-  )
-}
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
 
-export default Square
+export default Square;
