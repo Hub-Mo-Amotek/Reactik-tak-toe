aaf9c043ee116d2ade050814004a981111c0b109
Mo <Mohamed.elja@hotmail.com>
2024-07-09 14:50:19 +0200

    Adds square component with a state

---
+++ src/components/Square.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const Square = () => {
+
+    const [value, setValue] = useState(null)
+    
+
+    function handleClick() {
+        setValue('X');
+    }
+
+  return (
+    <div>
+          <button className="square"
+                  onClick={handleClick}
+          >{ value }</button>
+    </div>
+  )
+}
+
+export default Square
