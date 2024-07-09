c7f30e77d3d2f6061601c5bee074cfaa5148cbe3
Mo <Mohamed.elja@hotmail.com>
2024-07-09 19:29:38 +0200

    Adds history feature to the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
+const Board = ({ xIsNext, squares, onPlay }) => {
 
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setsquares] = useState(Array(9).fill(null));
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -26,28 +24,7 @@ const Board = () => {
       nextSquares[i] = "O";
     }
 
-    setsquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+    onPlay(nextSquares);
   }
 
 
@@ -72,5 +49,25 @@ const Board = () => {
     </div>
   )
 }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 export default Board
